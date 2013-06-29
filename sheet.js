3e20dafedbcf21be2455898ecc9f1f6f5eefd603
Jesse Huang <jh3141@gmail.com>
2013-06-29 15:35:42 -0400

    woefjwo

--- sheet.js
+++ sheet.js
@@ -1,36 +1,42 @@
 
-		var canvas = document.getElementById("sheet");
-		var renderer = new Vex.Flow.Renderer(canvas,
-			Vex.Flow.Renderer.Backends.CANVAS);
+var canvas = document.getElementById("sheet");
+var renderer = new Vex.Flow.Renderer(canvas,
+		Vex.Flow.Renderer.Backends.CANVAS);
 
-		var ctx = renderer.getContext();
-		var notes = [];
-
-
-		var stave = new Vex.Flow.Stave(50, 0, 980);
-		stave.addClef("treble").setContext(ctx).draw();
+var ctx = renderer.getContext();
+var notes = [];
 
 
-		function updateScore(key) {
+var stave = new Vex.Flow.Stave(50, 0, 980);
+stave.addClef("treble").setContext(ctx);
+//stave.addClef("treble").setContext(ctx).draw();
 
-			console.log(key);
-		stave = new Vex.Flow.Stave(50, 0, 980);
 
-		// Create the notes
+function updateScore(key) {
+
+	console.log(key);
+	stave = new Vex.Flow.Stave(0, 150, 1180);
+
+	// Create the notes
+	if (key[1]=="#") {
+		notes.push(new Vex.Flow.StaveNote({ keys: [key], duration: "q" }).addAccidental(0, new Vex.Flow.Accidental("#")));
+	}
+	else {
 		notes.push(new Vex.Flow.StaveNote({ keys: [key], duration: "q" }));
-			console.log(notes);
-//		var notes = [
-//		// A quarter-note C.
-//		new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" }),
-//
-//		// A quarter-note D.
-//		new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q" }),
-//
-//
-//			];
+	}
+	console.log(notes);
+	//		var notes = [
+	//		// A quarter-note C.
+	//		new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" }),
+	//
+	//		// A quarter-note D.
+	//		new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q" }),
+	//
+	//
+	//			];
 
-// Create a voice in 4/4
-var voice = new Vex.Flow.Voice({
+	// Create a voice in 4/4
+	var voice = new Vex.Flow.Voice({
 num_beats: notes.length,
 beat_value: 4,
 resolution: Vex.Flow.RESOLUTION
@@ -43,8 +49,8 @@ voice.addTickables(notes);
 var formatter = new Vex.Flow.Formatter().
 joinVoices([voice]).format([voice], 980);
 
-		ctx.clearRect(0,0,canvas.width, canvas.height);
-		stave.addClef("treble").setContext(ctx).draw();
+ctx.clearRect(0,0,canvas.width, canvas.height);
+stave.addClef("treble").setContext(ctx).draw();
 // Render voice
 voice.draw(ctx, stave);
 
